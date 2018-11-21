f948d97fc566f612475a8ac71601054dcc811d82
Matthew Hartley <mhartley@cantab.net>
2018-11-21 11:26:46 +0000

    Refactor to output TIFFs and tidy up

--- scripts/separate_nuclei.py
+++ scripts/separate_nuclei.py
@@ -6,7 +6,7 @@ import numpy as np
 
 import click
 
-from imageio import imread, imsave
+from imageio import imread, imsave, mimsave
 
 from natsort import natsorted
 
@@ -39,7 +39,19 @@ def im3d_to_fpath(fpath, im3d):
         )
 
 
-def im3d_from_fpath(fpath):
+def im3d_to_tiff(fpath, im3d):
+
+    root, ext = os.path.splitext(fpath)
+
+    assert ext in ['.tif', '.tiff']
+
+    # We use row, col, z, but mimsave expects z, row, col
+    transposed = np.transpose(im3d, axes=[2, 0, 1])
+
+    mimsave(fpath, transposed)
+
+
+def im3d_from_dirpath(fpath):
 
     fpath = Path(fpath)
 
@@ -50,26 +62,34 @@ def im3d_from_fpath(fpath):
     return np.dstack(ims)
 
 
-def spike_nuclei(image_series_dirpath):
+def find_name_root(dirpath):
 
-    im3d = im3d_from_fpath(image_series_dirpath)
+    return os.path.basename(os.path.normpath(dirpath))
+
+
+def find_objects(im3d):
 
     blurred = gaussian_filter(im3d, sigma=5)
-    im3d_to_fpath('scratch/blurred', blurred)
-
     global_thresh = threshold_otsu(blurred)
     thresholded = 255 * (blurred > global_thresh).astype(np.uint8)
-    im3d_to_fpath('scratch/thresholded', thresholded)
-
     connected_components = label(thresholded)
 
-    labels = list(np.unique(connected_components))
-    labels.remove(0)
+    return connected_components
 
-    l = 2
 
-    coords = np.where(connected_components == l)
+def scale_to_uint8(im3d):
+
+    imscaled = im3d.astype(np.float32)
+    imscaled = 255 * (imscaled - imscaled.min()) / (imscaled.max() - imscaled.min())
+    imscaled = imscaled.astype(np.uint8)
+
+    return imscaled
+
+
+def find_bounding_cube(coords):
+
     rcoords, ccoords, zcoords = coords
+
     rmin = rcoords.min()
     rmax = rcoords.max()
     cmin = ccoords.min()
@@ -77,21 +97,92 @@ def spike_nuclei(image_series_dirpath):
     zmin = zcoords.min()
     zmax = zcoords.max()
 
-    p = 5
+    return (rmin, rmax, cmin, cmax, zmin, zmax)
 
-    mask = np.where(connected_components != l)
-    imscaled = 255 * (im3d - im3d.min()) / (im3d.max() - im3d.min())
-    # im3d[mask] = 0
-    nuc = imscaled[rmin-p:rmax+p,cmin-p:cmax+p,zmin-p:zmax+p]
 
-    im3d_to_fpath('scratch/nuc{}'.format(l), nuc)
+def apply_bc_to_image(im3d, bc, xypad, zpad):
+
+    rdim, cdim, zdim = im3d.shape
+
+    rmin, rmax, cmin, cmax, zmin, zmax = bc
+    p_zmin = max(zmin - zpad, 0)
+    p_zmax = min(zmax + zpad, zdim)
+
+    return im3d[rmin-xypad:rmax+xypad,cmin-xypad:cmax+xypad,p_zmin:p_zmax]
+
+
+def extract_individual_nuclei(image_series_dirpath, output_dirpath):
+
+    name_root = find_name_root(image_series_dirpath)
+
+    im3d = im3d_from_dirpath(image_series_dirpath)
+
+    connected_components = find_objects(im3d)
+    labels = list(np.unique(connected_components))
+    labels.remove(0)
+
+    imscaled = scale_to_uint8(im3d)
+    for l in labels:
+        coords = np.where(connected_components == l)
+        print(len(coords[0]) * 0.04 * 0.04 * 0.11)
+        bc = find_bounding_cube(coords)
+        nuclear_region = apply_bc_to_image(imscaled, bc, xypad=20, zpad=8)
+        fname = "{}-nucleus{}.tif".format(name_root, l)
+        fpath = os.path.join(output_dirpath, fname)
+        im3d_to_tiff(fpath, nuclear_region)
+
+
+def spike_nuclei(image_series_dirpath):
+
+    im3d = im3d_from_dirpath(image_series_dirpath)
+
+    blurred = gaussian_filter(im3d, sigma=5)
+    im3d_to_tiff('scratch/blurred.tif', blurred)
+
+    global_thresh = threshold_otsu(blurred)
+    thresholded = 255 * (blurred > global_thresh).astype(np.uint8)
+    im3d_to_tiff('scratch/thresholded.tif', thresholded)
+
+    connected_components = label(thresholded)
+
+    labels = list(np.unique(connected_components))
+    labels.remove(0)
+
+    rdim, cdim, zdim = im3d.shape
+    xyp = 20
+    zp = 8
+    imscaled = im3d.astype(np.float32)
+    imscaled = 255 * (imscaled - imscaled.min()) / (imscaled.max() - imscaled.min())
+    imscaled = imscaled.astype(np.uint8)
+
+    for l in labels:
+        coords = np.where(connected_components == l)
+        rcoords, ccoords, zcoords = coords
+        rmin = rcoords.min()
+        rmax = rcoords.max()
+        cmin = ccoords.min()
+        cmax = ccoords.max()
+        zmin = zcoords.min()
+        zmax = zcoords.max()
+
+        mask = np.where(connected_components != l)
+
+        # FIXME - masking really needs the ROI dilated first
+        # imscaled[mask] = 0
+        p_zmin = max(zmin - zp, 0)
+        p_zmax = min(zmax + zp, zdim)
+        print(p_zmin, p_zmax)
+        nuc = imscaled[rmin-xyp:rmax+xyp,cmin-xyp:cmax+xyp,p_zmin:p_zmax]
+
+        im3d_to_tiff('scratch/nucleus{}.tif'.format(l), nuc)
 
 
 @click.command()
 @click.argument('image_series_dirpath')
-def main(image_series_dirpath):
+@click.argument('output_dirpath')
+def main(image_series_dirpath, output_dirpath):
 
-    spike_nuclei(image_series_dirpath)
+    extract_individual_nuclei(image_series_dirpath, output_dirpath)
 
 
 if __name__ == '__main__':
